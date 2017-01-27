ab12877a6a44c014d3cd420c74b617e6ac060675
Keri Sartain <kittenwarrior@gmail.com>
2017-01-26 20:58:41 -0600

    added verification for adding movie, escaping content of adding, added terrible movie verification

--- main.py
+++ main.py
@@ -101,16 +101,18 @@ class AddMovie(webapp2.RequestHandler):
         # look inside the request to figure out what the user typed
         new_movie = self.request.get("new-movie")
 
-        # TODO 2
         # if the user typed nothing at all, redirect and yell at them
+        if (not new_movie) or (new_movie.strip() == ""):
+            error= "You need to fill out a movie name: {0}".format(new_movie)
+            self.redirect("/?error=" + cgi.escape(error, quote=True))
 
-
-        # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
+        if new_movie in terrible_movies:
+            error = "You will not like '{0}' choose another movie.".format(new_movie)
+            self.redirect("/?error" + cgi.escae(error, quote=True))
 
-
-        # TODO 1
         # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        new_movie_escaped = cgi.escape(new_movie, quote=True)
 
         # build response content
         new_movie_element = "<strong>" + new_movie + "</strong>"
